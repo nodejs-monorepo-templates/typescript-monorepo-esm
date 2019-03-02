86cde8831e9d20e7858886e226f2daf1c33091af
khai96_ <hvksmr1996@gmail.com>
2019-03-03 02:24:12 +0700

    Prevent source JavaScript files from being renamed

--- tools/make-mjs/bin.js
+++ tools/make-mjs/bin.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 const path = require('path')
 const process = require('process')
-const { rename } = require('fs-extra')
+const { rename, stat } = require('fs-extra')
 const { traverse } = require('fs-tree-utils')
 const places = require('@tools/places')
 
@@ -15,10 +15,38 @@ async function main () {
 
   const renamingPromises = list
     .filter(x => x.stats.isFile())
-    .map(({ path: oldPath }) => ({ oldPath, ...path.parse(oldPath) }))
+    .map(({ path: oldPath }) => ({
+      oldPath,
+      ...path.parse(oldPath)
+    }))
     .filter(x => x.ext === '.js')
-    .map(({ oldPath, dir, name }) => ({ oldPath, newPath: path.join(dir, name + '.mjs') }))
-    .map(x => rename(x.oldPath, x.newPath).then(() => x))
+    .map(({ oldPath, dir, name }) => ({
+      oldPath,
+      newPath: path.join(dir, name + '.mjs'),
+      tsPath: path.join(dir, name + '.ts')
+    }))
+    .map(async param => {
+      const { oldPath, newPath, tsPath } = param
+
+      const statResult = await stat(tsPath).then(
+        stats => ({ stats }),
+        error => ({ error })
+      )
+
+      if ('stats' in statResult) {
+        if (!statResult.stats.isFile()) return param
+        await rename(oldPath, newPath)
+        return param
+      }
+
+      const { error } = statResult
+
+      if (typeof error === 'object' && error.code === 'ENOENT') {
+        return param
+      }
+
+      throw statResult.error
+    })
 
   await Promise.all(renamingPromises)
 
