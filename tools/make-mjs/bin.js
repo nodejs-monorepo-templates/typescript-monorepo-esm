4d4b56c97141e5f1d49a26d9520ab0899fefd1f8
khai96_ <hvksmr1996@gmail.com>
2019-09-16 13:38:13 +0700

    Customize isMjsPackage

--- tools/make-mjs/bin.js
+++ tools/make-mjs/bin.js
@@ -3,15 +3,32 @@ const path = require('path')
 const process = require('process')
 const fsx = require('fs-extra')
 const proceed = require('@make-mjs/main').main
+const { DEFAULT_PARSER_OPTIONS } = require('@make-mjs/file')
 const places = require('@tools/places')
 
 async function main () {
   const IGNORED_DIRECTORIES = ['.git', 'node_modules']
 
+  const knownMjsPackagesPromises = (
+    await fsx.readdir(places.packages)
+  ).map(async basename => {
+    const manifest = path.join(places.packages, basename, 'package.json')
+    const manifestExists = await fsx.pathExists(manifest)
+    if (!manifestExists) return null
+    const { name } = await fsx.readJSON(manifest)
+    return name
+  })
+
+  const knownMjsPackages = (await Promise.all(knownMjsPackagesPromises)).filter(Boolean)
+
   const events = proceed({
     dirname: places.packages,
     deep: param => !IGNORED_DIRECTORIES.includes(param.base),
-    filter: param => param.base.endsWith('.js')
+    filter: param => param.base.endsWith('.js'),
+    codeTransformOptions: {
+      parserOptions: DEFAULT_PARSER_OPTIONS,
+      isMjsPackage: param => knownMjsPackages.includes(param.packageName)
+    }
   })
 
   for await (const event of events) {
